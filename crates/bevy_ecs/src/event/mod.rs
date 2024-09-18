077361e9a40d53afec358b5a5651ec24c45cc349
Navneet Aman <contact@navneetaman.com>
2024-09-18 14:16:19 +0530

    wip

--- crates/bevy_ecs/src/event/mod.rs
+++ crates/bevy_ecs/src/event/mod.rs
@@ -1,4 +1,6 @@
-use crate::{self as bevy_ecs, world::Mut};
+use core::any::TypeId;
+
+use crate::{system::{IntoSystem, System}, world::Mut};
 use bevy_utils::TypeIdMap;
 use smallvec::SmallVec;
 
@@ -6,6 +8,7 @@ use crate::{system::BoxedSystem, world::World};
 
 pub struct EventInSystem<E: Event + Clone> {
     pub v: BoxedSystem<E, Box<dyn Event>>,
+    pub tid: TypeId,
 }
 pub struct RegisteredSystems<E: Event + Clone>{
     pub v: SmallVec<[EventInSystem<E>; 1]>,
@@ -15,6 +18,21 @@ pub struct RegisteredSystems<E: Event + Clone>{
 pub trait Event: Send + Sync + 'static {
     fn run_systems(&self, world: &mut World);
 }
+pub fn register_system<E, Out, F, M>(world: &mut World, f: F)
+where
+    E: Event + Clone,
+    Out: Event + Clone,
+    F: IntoSystem<E, Out, M> + 'static,
+{
+    world.init_resource::<RegisteredSystems<E>>();
+    world.resource_scope(|world: &mut World, mut systems: Mut<RegisteredSystems<E>>| {
+        let tid = TypeId::of::<F>();
+        if systems.tid.contains_key(&tid) { return };
+
+        let system = IntoSystem::into_system(f);
+    });
+}
+
 impl<E> Event for E
 where E: Send + Sync + Clone + 'static
 {
